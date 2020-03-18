db7568f2a99f22191be97a61f22fddf3a1d855e5
Phillip <phillip.bomholtz@gmail.com>
2020-03-18 11:39:52 +0100

    Dummy setup

--- Test_files/Card_Recognizer.py
+++ Test_files/Card_Recognizer.py
@@ -1,2 +1,24 @@
 
-#this is a test :)
+####################################################
+#          Test for tennisball collectror          #
+#                                                  #
+# version 0.0                                      #
+# description: A test file for finding cards in    #
+#              openCV 2                            #
+#												   #
+# auther:       Phillip Bomholtz                   #
+# created:      18-03-2020						   #
+# last updated: 18-03-2020						   #
+#												   #
+####################################################
+
+
+import cv2
+import numpy as np 
+import os
+
+cap = cv2.VideoCapture(1);  #setup video capture. might need to change to 0 depenting on own setup
+
+#set resolution to 1920x1080
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920);
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080);
